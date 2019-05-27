6fad90a3d610c2454f4b0a23d686fcf813df0d12
lixiaoqi <lixiaoqi@nbhzhd.com>
2019-05-27 17:32:51 +0800

    完善模版

--- Snake/GameController.h
+++ Snake/GameController.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include <Windows.h>
+#include <iostream>
+
+class StateControl {
+private:
+
+public:
+
+protected:
+
+};
+
+class ActionControl {
+private:
+
+public:
+
+protected:
+
+};
+
+class RuleControl {
+private:
+
+public:
+
+protected:
+
+};
